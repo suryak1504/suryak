4cfc322fa8d0541947bf36da7533b48265fd86af
Surya Kandregula <surkandr@in.ibm.com>
2019-09-16 07:05:35 +0000

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    *** Add function  ***
+     return x + y
+
+
+ def substract (x, y):
+    *** Substract function ***
+    return x - y
+
+ def multiply (x, y):
+     *** Multiply function ***
+     return x * y
+
+ def divide (x, y):
+     *** Divide function  ***
+     return x / y
